4228547b29f6a9b5048fe27697da084a43953bf9
Ayush Harsh <er.hi.harshsrivastava@gmail.com>
2026-05-25 02:00:37 +0530

    Update main.tf

--- 02-iac-aws-terraform/modules/rds/main.tf
+++ 02-iac-aws-terraform/modules/rds/main.tf
@@ -10,7 +10,7 @@ resource "aws_db_subnet_group" "main" {
   }
 }
 
-resource "aws_security_group" "rds" {
+resource "aws_security_group" "rds" { 
   name   = "${var.project_name}-${var.environment}-rds-sg"
   vpc_id = var.vpc_id
 
